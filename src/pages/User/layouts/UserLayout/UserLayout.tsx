d89d45f09e61728a04979a3a7cea604ff2936669
Anh Tuan <tuantrungvuongk62@gmail.com>
2024-01-14 18:24:10 +0700

    UI Profile

--- src/pages/User/layouts/UserLayout/UserLayout.tsx
+++ src/pages/User/layouts/UserLayout/UserLayout.tsx
@@ -5,13 +5,13 @@ export default function UserLayout() {
   return (
     <div className='bg-neutral-100 py-16 text-sm text-gray-600'>
       <div className='container'>
-        <div className='grid grid-cols-1  gap-6 md:grid-cols-12'>
+        <div className='grid grid-cols-1 gap-6 md:grid-cols-12'>
           <div className='md:col-span-3 lg:col-span-2'>
             <UserSideNav />
           </div>
-        </div>
-        <div className='md:col-span-9 lg:col-span-10'>
-          <Outlet />
+          <div className='md:col-span-9 lg:col-span-10'>
+            <Outlet />
+          </div>
         </div>
       </div>
     </div>
