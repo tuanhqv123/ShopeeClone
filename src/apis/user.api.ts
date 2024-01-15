bbcc4d243e1ac6298052a53e3a7d83cc34adf409
Anh Tuan <tuantrungvuongk62@gmail.com>
2024-01-15 19:52:16 +0700

    Api User

---
+++ src/apis/user.api.ts
@@ -0,0 +1,24 @@
+import { User } from '~/types/user.type'
+import { SuccessResponse } from '~/types/utils.type'
+import http from '~/utils/http'
+
+interface BodyUpdateProfile extends Omit<User, '_id' | 'roles' | 'createdAt' | 'updatedAt' | 'email'> {
+  password?: string
+  newpassword?: string
+}
+
+const userApi = {
+  getProfile() {
+    return http.get<SuccessResponse<User>>('me')
+  },
+  updateProfile(body: BodyUpdateProfile) {
+    return http.put<SuccessResponse<User>>('user', body)
+  },
+  uploadAvatar(body: FormData) {
+    return http.post<SuccessResponse<string>>('user/upload-avatar', body, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+  }
+}
